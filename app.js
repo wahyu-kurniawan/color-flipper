c48d76547393085f0ba655bf00ab04615e5713b4
wahyu-kurniawan <wahyukurniawan616@gmail.com>
2020-09-30 13:55:08 +0700

    membuat markup body untu hex.html

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const colors = ["green", "red", "rgba(145,220,150", "#f2045"];
+const colors = ["green", "red", "rgba(145,220,150)", "#f2045"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 
