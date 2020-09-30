69da7d65a5c33a65a34822e4163ba0c09c39e1cb
wahyu-kurniawan <wahyukurniawan616@gmail.com>
2020-09-30 13:41:48 +0700

    menambahkan even pada button

---
+++ app.js
@@ -0,0 +1,7 @@
+const colors = ["green", "red", "rgba(145,220,150", "#f2045"];
+const btn = document.getElementById("btn");
+const color = document.querySelector(".color");
+
+btn.addEventListener("click", () => {
+  const randomNumber = 2;
+});
